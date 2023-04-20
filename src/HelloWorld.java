4711daba8f1dcac799e927ab00d483d956c90491
UserLightMode <lightbendingupwards@proton.me>
2023-04-20 02:50:26 -0500

    First version

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * First file to be uploaded to GitHub. I am not sure what GitHub is at the moment, but from what
+ * I have been reading it seems to be a way for developers to share their code in a way that also
+ * tracks the history of the changes for each project. My goal is to upload my code to GitHub to
+ * show my journey of learning how to code.
+ * <p>
+ * Author: Jacob Hall
+ * Date: 04/20/2023
+ */
+
+// Class name must be the same as the filename
+public class HelloWorld {
+    // Every Java program must have a main() which is where code will start to be executed
+    public static void main(String[] args) {
+        // The System.out.println has something to do with methods
+        // Just have to learn a little bit more about the structure to fully understand
+        System.out.println("Hello World");
+    }
+}
